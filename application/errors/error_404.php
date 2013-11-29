ec7e5db472d02db7c2efd9d4f82ef6a269e04dff
Lialosiu <lialosiu@gmail.com>
2013-11-29 13:08:40 +0800

    合并郑旭的404页面

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,62 +1,124 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<title>404 Page Not Found</title>
-<style type="text/css">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <title>404</title>
+    <style type="text/css">
+        body {
+            margin: 0;
+            padding: 0;
+            font: 14px/1.6 Arial, Sans-serif;
+            background: #fff repeat-x;
+        }
 
-::selection{ background-color: #E13300; color: white; }
-::moz-selection{ background-color: #E13300; color: white; }
-::webkit-selection{ background-color: #E13300; color: white; }
+        a:link, a:visited {
+            color: #007ab7;
+            text-decoration: none;
+        }
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+        h1 {
+            position: relative;
+            z-index: 2;
+            width: 540px;
+            height: 0;
+            margin: 110px auto 15px;
+            padding: 230px 0 0;
+            overflow: hidden;
+            border: 1px solid;
+            background-image: url(<?php echo base_url('public/images/404.jpg');?>);
+        }
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+        h2 {
+            position: absolute;
+            top: 55px;
+            left: 233px;
+            margin: 0;
+            font-size: 0;
+            text-indent: -999px;
+            -moz-user-select: none;
+            -webkit-user-select: none;
+            user-select: none;
+            cursor: default;
+            width: 404px;
+            height: 90px;
+        }
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+        h2 em {
+            display: block;
+            font: italic bold 200px/120px "Times New Roman", Times, Serif;
+            text-indent: 0;
+            letter-spacing: -5px;
+            color: rgba(216, 226, 244, 0.3);
+        }
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+        .link a {
+            margin-right: 1em;
+        }
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
-}
+        .link, .texts {
+            width: 540px;
+            margin: 0 auto 15px;
+            color: #505050;
+        }
 
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+        .texts {
+            line-height: 2;
+        }
+
+        .texts dd {
+            margin: 0;
+            padding: 0 0 0 15px;
+        }
+
+        .texts ul {
+            margin: 0;
+            padding: 0;
+        }
+
+        .portal {
+            color: #505050;
+            text-align: center;
+            white-space: nowrap;
+            word-spacing: 0.45em;
+        }
+
+        .portal a:link, .portal a:visited {
+            color: #505050;
+            word-spacing: 0;
+        }
+
+        .portal a:hover, .portal a:active {
+            color: #007ab7;
+        }
+
+        .portal span span span {
+            padding: 0 20px 0 0;
+            background-position: 100% -80px;
+        }
+
+        .STYLE1 {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 65px;
+        }
+    </style>
 </head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+<h1></h1>
+
+<p class="link">
+    <a href="<?php echo base_url(); ?>">&#9666;返回首页</a>
+    <a href="javascript:history.go(-1);">&#9666;返回上一页</a>
+</p>
+<dl class="texts">
+    <dt>我们正在联系火星总部查找您所需要的页面.请返回等待信息..</dt>
+    <dd>
+        <ul>
+            <li>不要返回吗?</li>
+            <li>确定不要返回吗?</li>
+            <li>真的真的确定不要返回吗?</li>
+            <li>好吧.还是随便你要不要真的确定返回吧</li>
+            <li>请原谅我无耻的卖萌~~~~~~快回去继续刷题吧!!</li>
+        </ul>
+    </dd>
+</dl>
 </body>
 </html>
