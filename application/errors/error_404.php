6de909e9c555899dde9c6622c868d09a63b04586
Lialosiu <lialosiu@gmail.com>
2013-11-29 18:39:42 +0800

    错误页编码utf8

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,6 +1,7 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>404</title>
     <style type="text/css">
         body {
