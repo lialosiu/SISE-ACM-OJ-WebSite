2460c697250c180a9aadab4e4d0e454086e9772b
Lialosiu <lialosiu@gmail.com>
2013-11-29 17:26:46 +0800

    比赛题除管理员外不显示代码长度

--- application/views/home/answer-list.php
+++ application/views/home/answer-list.php
@@ -31,35 +31,35 @@
             <tbody>
             <?php if (isset ($thatAnswerList)) : ?>
                 <?php foreach ($thatAnswerList->getAnswerArray() as $thisAnswer) : ?>
-                    <tr <?php if ($CurrentUser->isAdministrator() || $thisAnswer->getUserID() == $CurrentUser->getID()) echo 'onclick="location.href=\'' . base_url('home/showAnswer/' . $thisAnswer->getID()) . '\'"'?>)>
-                    <td><?php echo $thisAnswer->getID(); ?></td>
-                    <td><a href="<?php echo base_url('home/showProblem/' . $thisAnswer->getProblemID()); ?>"><?php echo $thisAnswer->getProblemID(); ?></a></td>
-                    <td><a href="<?php echo base_url('home/showUser/' . $thisAnswer->getUserID()); ?>"><?php echo $thisAnswer->getUser()->getNickname(); ?></a></td>
-                    <td><?php echo $thisAnswer->getLanguage() ?></td>
-                    <td><?php echo $thisAnswer->getUsedTime() ?></td>
-                    <td><?php echo $thisAnswer->getUsedMemory(); ?></td>
-                    <td><?php echo strlen($thisAnswer->getSourceCode()); ?></td>
-                    <td><?php
-                        switch ($thisAnswer->getStatusCode()) {
-                            case _StatusCode_Accepted:
-                                echo '<span class="text-danger">' . $thisAnswer->getStatus() . '</span>';
-                                break;
-                            case _StatusCode_CompileError:
-                            case _StatusCode_RuntimeError:
-                                echo '<a title="' . $thisAnswer->getInfo() . '">' . $thisAnswer->getStatus() . '</a>';
-                                break;
-                            default:
-                                echo $thisAnswer->getStatus();
-                                break;
-                        }
-                        ?></td>
-                    <?php if (isset($CurrentUser) && $CurrentUser->isAdministrator()): ?>
-                        <td>
-                            <a class="btn btn-primary btn-xs" href="<?php echo base_url('api/recheckAnswer/' . $thisAnswer->getID()); ?>">重判</a>
-                        </td>
-                    <?php endif; ?>
-                </tr>
-            <?php endforeach; ?>
+                    <tr <?php if ($CurrentUser->isAdministrator() || $thisAnswer->getUserID() == $CurrentUser->getID()) echo 'onclick="location.href=\'' . base_url('home/showAnswer/' . $thisAnswer->getID()) . '\'"'; ?>>
+                        <td><?php echo $thisAnswer->getID(); ?></td>
+                        <td><a href="<?php echo base_url('home/showProblem/' . $thisAnswer->getProblemID()); ?>"><?php echo $thisAnswer->getProblemID(); ?></a></td>
+                        <td><a href="<?php echo base_url('home/showUser/' . $thisAnswer->getUserID()); ?>"><?php echo $thisAnswer->getUser()->getNickname(); ?></a></td>
+                        <td><?php echo $thisAnswer->getLanguage() ?></td>
+                        <td><?php echo $thisAnswer->getUsedTime() ?></td>
+                        <td><?php echo $thisAnswer->getUsedMemory(); ?></td>
+                        <td><?php if ($thisAnswer->getProblem()->getContestID() != 0 && !$CurrentUser->isAdministrator()) echo '[-比赛题-]'; else echo strlen($thisAnswer->getSourceCode()); ?></td>
+                        <td><?php
+                            switch ($thisAnswer->getStatusCode()) {
+                                case _StatusCode_Accepted:
+                                    echo '<span class="text-danger">' . $thisAnswer->getStatus() . '</span>';
+                                    break;
+                                case _StatusCode_CompileError:
+                                case _StatusCode_RuntimeError:
+                                    echo '<a title="' . $thisAnswer->getInfo() . '">' . $thisAnswer->getStatus() . '</a>';
+                                    break;
+                                default:
+                                    echo $thisAnswer->getStatus();
+                                    break;
+                            }
+                            ?></td>
+                        <?php if (isset($CurrentUser) && $CurrentUser->isAdministrator()): ?>
+                            <td>
+                                <a class="btn btn-primary btn-xs" href="<?php echo base_url('api/recheckAnswer/' . $thisAnswer->getID()); ?>">重判</a>
+                            </td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endforeach; ?>
             <?php endif; ?>
             </tbody>
         </table>
